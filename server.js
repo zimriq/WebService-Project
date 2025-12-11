30cb6eec1cfbe81fea292e879897a292d54fe461
Zimri Quinonez <zimryquinonez@gmail.com>
2025-12-11 12:00:34 -0500

    adding authorization header issue

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ function hashPassword(password){
     return crypto.createHash('sha256').update(password).digest('hex');
 }
 
+function verifyingPlatformKey(req, res, next){
+    const platformKey = req.headers['eleos-platform-key'];
+    if(platformKey !== process.env.ELEOS_PLATFORM_KEY){
+        return res.status(401).json({error: 'Unauthorized due to missing or invalid Platform Key'});
+    }
+    next();
+}
+
 console.log('Mongo URI:', process.env.MONGODB_URI);
 
 const connectDB = async () => {
@@ -38,7 +46,7 @@ app.get('/', (req, res) => {
 //authentication 
 //Expose an authentication endpoint that takes in a POST request with a username and passowrd and returns a user object 
 //  or an unauthorized response if username/password is invalid
-app.post('/authenticate', async (req, res) => {
+app.post('/authenticate', verifyingPlatformKey, async (req, res) => {
     try{
         const clientIp = req.headers['x-forwarded-for'];
 
@@ -70,7 +78,7 @@ app.post('/authenticate', async (req, res) => {
 
 //Expose an authentication API endpoint that takes in a GET request that passes an API token returned from above POST 
 //  request and then also returns a user object (or an uhthorized response if token is invalid)
-app.get('/authenticate/:token', async (req, res) => {
+app.get('/authenticate/:token', verifyingPlatformKey, async (req, res) => {
     try{
         const clientIp = req.headers['x-forwarded-for'];
 
@@ -99,7 +107,7 @@ app.get('/authenticate/:token', async (req, res) => {
 //loads 
 //Expose a loads API endpoint that takes in an api_token to authenticate/associate which user
 // and returns loads for that user
-app.get('/loads', async (req, res) => {
+app.get('/loads', verifyingPlatformKey, async (req, res) => {
     try{
         const authorization = req.headers['authorization'];
         if(!authorization){
